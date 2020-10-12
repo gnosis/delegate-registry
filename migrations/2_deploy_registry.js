68e81fb8fde934331dd10946d6df7779034480f7
Richard Meissner <richard@gnosis.io>
2020-10-12 11:53:36 +0200

    Clean up migration

--- migrations/2_deploy_registry.js
+++ migrations/2_deploy_registry.js
@@ -1,7 +1,5 @@
 var DelegateRegistry = artifacts.require("./DelegateRegistry.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(DelegateRegistry).then(function (safe) {
-        return safe
-    });
+    deployer.deploy(DelegateRegistry)
 };
