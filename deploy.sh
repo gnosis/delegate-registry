0dc80f26a703727a5ca2df69b03d97c8ba7b4660
Richard Meissner <msc.meissner@gmail.com>
2020-11-09 21:05:48 +0100

    Update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ echo $'\nNETWORK='$1 >> .env.deploy
 docker-compose -f docker-compose.yml -f docker-compose.deploy.yml --env-file .env.deploy up --build
 
 node scripts/clean_build.js
+node scripts/generate_meta.js --upload
+yarn truffle exec scripts/verify_deployment.js --network=$1
 
 rm .env.deploy
