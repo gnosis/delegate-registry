67f68a7da50d928579c20f217b580fac059fdb15
Asgeir <asgeir@cluda.com>
2023-02-01 15:09:15 +0100

    Consistent package naming + license

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,5 +1,5 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { SetDelegate } from "../generated/DelegateRegistry/DelegateRegistry";
-import { To, From, Delegation } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts"
+import { SetDelegate } from "../generated/DelegateRegistry/DelegateRegistry"
+import { To, From, Delegation } from "../generated/schema"
 
 export function handleDelegation(event: SetDelegate): void {}
