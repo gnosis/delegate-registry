b01b256f76e4f9a38b40fae70c66f284aad926b1
Asgeir <asgeir@cluda.com>
2023-02-01 14:49:17 +0100

    Init subgraph package

---
+++ packages/subgraph/src/mapping.ts
@@ -0,0 +1,5 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { SetDelegate } from "../generated/DelegateRegistry/DelegateRegistry";
+import { To, From, Delegation } from "../generated/schema";
+
+export function handleDelegation(event: SetDelegate): void {}
