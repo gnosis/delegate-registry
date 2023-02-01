002fb23e3006ac84747fca1c4b995f1ce6a4be44
Asgeir <asgeir@cluda.com>
2023-02-01 14:03:45 +0100

    Update hardhat config, making it possible to verify contracts

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
