08169f89e290ea20ef57b59bfb0507aed33d6a17
Auryn Macmillan <hi@auryn.xyz>
2023-02-01 14:09:58 +1100

    Ready to write tests

--- contracts/DelegateRegistryBytes32.sol
+++ contracts/DelegateRegistryBytes32.sol
@@ -3,21 +3,22 @@ pragma solidity ^0.8.17;
 
 struct Delegation {
     bytes32 id;
-    uint64 ratio;
-    uint64 term;
+    uint256 ratio;
 }
 
 contract DelegateRegistry {
-    // The first key is the delegator and the second key a id.
-    // The value is the delegation information.
-    mapping(address => mapping(string => Delegation[])) public delegations;
+    // Mapping from delegator address => context ID => array of delegations.
+    mapping(address => mapping(string => Delegation[])) private delegations;
+    // Mapping from delegator address => context ID => user-defined delegation expiration dates.
+    mapping(address => mapping(string => uint256)) private expirationTimestamps;
 
     // Using these events it is possible to process the events to build up reverse lookups.
     // The indeces allow it to be very partial about how to build this lookup (e.g. only for a specific delegate).
     event SetDelegate(
         address indexed delegator,
         string indexed id,
-        Delegation[] delegation
+        Delegation[] delegation,
+        uint256 expiration
     );
 
     /// @dev Delegation is already set to this value.
@@ -27,22 +28,33 @@ contract DelegateRegistry {
     ///      The combination of msg.sender and the id can be seen as a unique key.
     /// @param id Id for which the delegate should be set.
     /// @param delegation Array of delegations.
+    /// @param expirationTimestamp 64-bit Unix timestamp for the date at which this expiration should expire.
     function setDelegate(
         string memory id,
-        Delegation[] memory delegation
+        Delegation[] memory delegation,
+        uint256 expirationTimestamp
     ) public {
-        Delegation[] memory currentDelegation = delegations[msg.sender][id];
+        // Delegation[] memory currentDelegation = delegations[msg.sender][id];
         bytes32 currentDelegationHash = keccak256(
-            abi.encode(currentDelegation)
+            abi.encode(delegations[msg.sender][id])
         );
         bytes32 delegationHash = keccak256(abi.encode(delegation));
 
-        if (currentDelegationHash == delegationHash)
-            revert DuplicateDelegation();
+        if (
+            currentDelegationHash == delegationHash &&
+            expirationTimestamps[msg.sender][id] == expirationTimestamp
+        ) revert DuplicateDelegation();
+
+        delete delegations[msg.sender][id];
 
         // Update delegation mapping
-        delegations[msg.sender][id] = delegation;
+        for (uint i = 0; i < delegation.length; i++) {
+            delegations[msg.sender][id][i] = delegation[i];
+        }
 
-        emit SetDelegate(msg.sender, id, delegation);
+        // set delegation expiration
+        expirationTimestamps[msg.sender][id] = expirationTimestamp;
+
+        emit SetDelegate(msg.sender, id, delegation, expirationTimestamp);
     }
 }
