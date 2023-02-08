75d178afd723fb8ad04899fd7aa569c31dc70c5f
Auryn Macmillan <hi@auryn.xyz>
2023-02-09 09:12:13 +1100

    add optimizer

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -1,12 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import dotenv from "dotenv"
 
-dotenv.config();
-const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env;
+dotenv.config()
+const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
@@ -15,6 +23,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
-};
+}
 
-export default config;
+export default config
