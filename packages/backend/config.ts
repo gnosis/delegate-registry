ea0c88b213f1e49388834823074fc4903f1a383c
Asgeir <asgeir@cluda.com>
2023-02-09 10:42:38 +0100

    [backend] Make strategies easily configurable

---
+++ packages/backend/config.ts
@@ -0,0 +1,22 @@
+export const strategies = [
+  {
+    name: "gno",
+    network: "1",
+    params: {
+      symbol: "GNO",
+      decimals: 18,
+      SUBGRAPH_URL:
+        "https://api.thegraph.com/subgraphs/id/QmYNFPz2j1S8wdm2nhou6wRhGXfVVFzVi37LKuvcHBayip",
+    },
+  },
+  {
+    name: "gno",
+    network: "100",
+    params: {
+      symbol: "GNO",
+      decimals: 18,
+      SUBGRAPH_URL:
+        "https://api.thegraph.com/subgraphs/id/QmduKVUHCPjR5tmNEgooXHBMGKqDJWrUPdp6dEMeJM6Kqa",
+    },
+  },
+]
--- packages/backend/lib/services/snapshot-api.ts
+++ packages/backend/lib/services/snapshot-api.ts
@@ -1,24 +1,32 @@
 import snapshot from "@snapshot-labs/snapshot.js"
+import { strategies } from "../../config"
+import * as R from "ramda"
 
 const space = process.env.SNAPSHOT_SPACE
 if (space == null) {
   throw Error("SNAPSHOT_SPACE is not defined")
 }
 
-const strategies = [
-  {
-    name: "erc20-balance-of",
-    params: {
-      address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      symbol: "DAI",
-      decimals: 18,
-    },
-  },
-]
-const network = "1"
-
 export const getVoteWeights = async (
   addresses: string[],
   blockNumber?: number,
 ) =>
-  snapshot.utils.getScores(space, strategies, network, addresses, blockNumber)
+  strategies.reduce(async (acc, strategy) => {
+    if (Object.keys(acc).length === 0) {
+      return await snapshot.utils.getScores(
+        space,
+        [strategy],
+        strategy.network,
+        addresses,
+        blockNumber,
+      )
+    }
+    const scores = await snapshot.utils.getScores(
+      space,
+      [strategy],
+      strategy.network,
+      addresses,
+      blockNumber,
+    )
+    return R.mergeWith(R.add, acc, scores)
+  }, {})
