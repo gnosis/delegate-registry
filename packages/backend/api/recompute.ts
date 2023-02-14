3a56aba7bc34decf4d973c7720eb9ee8fd86d635
Asgeir <asgeir@cluda.com>
2023-02-14 12:02:48 +0100

    [backend] encapsulate retrieval logic + make sure to just request voting power from Snapshot for unique addresses

--- packages/backend/api/recompute.ts
+++ packages/backend/api/recompute.ts
@@ -1,11 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
+import R from "ramda"
 import { computeDelegatedVoteWeights } from "../lib/compute-vote-weights"
 import { getVoteWeights } from "../lib/services/snapshot-api"
 import * as storage from "../lib/services/storage"
 import { getAllDelegationsTo as getAllRepresentatives } from "../lib/services/the-graph"
-import { utils } from "ethers"
-
-const { getAddress } = utils
 
 /**
  * Recomputes the vote weights for all delegations, and stores the results.
@@ -19,21 +17,8 @@ export default async function getDelegations(
   console.log("1. Fetch and merge all delegations across all chains")
 
   // 1.1. - get all to delegations
-  const representatives = await getAllRepresentatives()
+  const delegations = await getAllRepresentatives()
 
-  const delegations = representatives.data?.tos.reduce(
-    (acc, { id: representative, delegations }) => {
-      acc[getAddress(representative.slice(-40))] = delegations.reduce(
-        (acc, { from: { id: delegator }, ratio }) => {
-          acc[getAddress(delegator)] = ratio
-          return acc
-        },
-        {} as Record<string, number>,
-      )
-      return acc
-    },
-    {} as Record<string, Record<string, number>>,
-  )
   if (delegations == null) {
     console.log("Done: no delegations found")
     return response.status(200).json({
@@ -42,18 +27,18 @@ export default async function getDelegations(
   }
 
   console.log("2. Get vote weights for all delegators")
-  const delegators = Object.keys(delegations).reduce(
-    (representatives, representative) => [
-      ...representatives,
-      ...Object.keys(delegations[representative]),
-    ],
-    [] as string[],
+  const delegators = R.uniq(
+    Object.keys(delegations).reduce(
+      (representatives, representative) => [
+        ...representatives,
+        ...Object.keys(delegations[representative]),
+      ],
+      [] as string[],
+    ),
   )
-
   const voteWeights = await getVoteWeights(delegators)
 
   console.log("3. Compute vote weights for all delegations")
-
   const delegatedVoteWeight = computeDelegatedVoteWeights(
     delegations,
     voteWeights,
--- packages/backend/lib/services/the-graph.ts
+++ packages/backend/lib/services/the-graph.ts
@@ -1,5 +1,26 @@
 import { ExecutionResult } from "graphql"
 import { execute, GetTosDocument, GetTosQuery } from "../../.graphclient"
+import { utils } from "ethers"
 
-export const getAllDelegationsTo = (): Promise<ExecutionResult<GetTosQuery>> =>
-  execute(GetTosDocument, {})
+const { getAddress } = utils
+
+export const getAllDelegationsTo = async () => {
+  const responds: ExecutionResult<GetTosQuery> = await execute(
+    GetTosDocument,
+    {},
+  )
+
+  return responds.data?.tos.reduce(
+    (acc, { id: representative, delegations }) => {
+      acc[getAddress(representative.slice(-40))] = delegations.reduce(
+        (acc, { from: { id: delegator }, ratio }) => {
+          acc[getAddress(delegator)] = ratio
+          return acc
+        },
+        {} as Record<string, number>,
+      )
+      return acc
+    },
+    {} as Record<string, Record<string, number>>,
+  )
+}
