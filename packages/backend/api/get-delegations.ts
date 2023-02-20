972c46261651afd85fecb0ea55d49a1c8a735ea6
Asgeir <asgeir@cluda.com>
2023-02-20 11:20:32 +0100

    [backend] Add `get-delegations?space=` endpoint
    With the format supported by the API POST strategy (https://github.com/snapshot-labs/snapshot-strategies/tree/master/src/strategies/api-post)

--- packages/backend/api/get-delegations.ts
+++ packages/backend/api/get-delegations.ts
@@ -1,19 +1,49 @@
 import { getDelegatedVoteWeight } from "../lib/services/storage/read"
+import { get } from "@vercel/edge-config"
+import * as R from "ramda"
+import { utils } from "ethers"
+const { getAddress } = utils
+
 /**
  * To be called by the "API POST strategy" snapshot strategy:
  * https://github.com/snapshot-labs/snapshot-strategies/tree/master/src/strategies/api-post
  */
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 }
 
 export default async (req: Request) => {
   const body = await req.json()
-  console.log("Request body: ", body)
-  const { addresses, snapshotSpace } = body
+  const url = new URL(req.url)
+  const snapshotSpace = url.searchParams.get("space")
+  if (snapshotSpace == null) {
+    return new Response("Missing `space` parameter (Snapshot space).", {
+      status: 400,
+    })
+  }
+  let { addresses } = body as { addresses: string[] }
+  try {
+    addresses = R.map(getAddress, addresses)
+  } catch (e) {
+    console.log("Error parsing addresses: ", e)
+    return new Response(
+      "The `addresses` parameter is required with an array of valid addresses.",
+      { status: 400 },
+    )
+  }
 
   const voteWeights = await getDelegatedVoteWeight(snapshotSpace)
-  console.log("greeting", voteWeights)
+  if (voteWeights == null || R.isEmpty(voteWeights)) {
+    console.log("No vote weights found for space: ", snapshotSpace)
+    return new Response(JSON.stringify({ score: [] }), { status: 200 })
+  }
 
-  return new Response(JSON.stringify({ score: voteWeights }))
+  const relevantVoteWeights = R.pickAll(addresses, voteWeights)
+
+  const relevantVoteWightsAsScores = R.map(
+    ([address, score = 0]) => ({ address, score }),
+    R.toPairs(relevantVoteWeights),
+  )
+
+  return new Response(JSON.stringify(relevantVoteWightsAsScores))
 }
--- packages/backend/lib/services/storage/read.ts
+++ packages/backend/lib/services/storage/read.ts
@@ -1,6 +1,7 @@
 // To be used by the Vercel Edge Function
 import { get } from "@vercel/edge-config"
 
-export const getDelegatedVoteWeight = async (snapshotSpace: string) => {
-  return get(snapshotSpace)
-}
+export const getDelegatedVoteWeight = (snapshotSpace: string) =>
+  get<{
+    [delegate: string]: number
+  }>(`${snapshotSpace.replace(".", "_")}-delegatedVoteWeight`)
