529631be9d1a7e6a5c335b088b41d55533ef67fb
Asgeir <asgeir@cluda.com>
2023-02-22 10:35:30 +0100

    [backend] Fix codegen typings

--- packages/backend/lib/services/the-graph/resolvers.ts
+++ packages/backend/lib/services/the-graph/resolvers.ts
@@ -4,13 +4,13 @@ export const resolvers: Resolvers = {
   Context: {
     // chainName can exist already in root as we pass it in the other resolver
     chainName: (root, args, meshContext, info) =>
-      root.chainName || (meshContext as any).chainName || "gorli", // The value we provide in the config
+      root.chainName || meshContext.chainName || "gorli", // The value we provide in the config
   },
   Query: {
     crossContext: async (root, args, meshContext, info) =>
       Promise.all(
         args.chainNames.map((chainName: string) =>
-          (meshContext as any).DelegateRegistry.Query.contexts({
+          meshContext.DelegateRegistry.Query.contexts({
             root,
             args,
             context: {
@@ -30,7 +30,7 @@ export const resolvers: Resolvers = {
     crossContexts: async (root, args, meshContext, info) =>
       Promise.all(
         args.chainNames.map((chainName: string) =>
-          (meshContext as any).DelegateRegistry.Query.contexts({
+          meshContext.DelegateRegistry.Query.contexts({
             root,
             args,
             context: {
