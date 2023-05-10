ee05719cee5ed03e434bf0974ff7d50f28a837f8
Asgeir <asgeir@cluda.com>
2023-05-10 10:33:01 +0200

    Add endpoint exposing a delegate
    `api/{context/snapshotSpace}/latest/delegates/{delegateAddress}`

--- packages/backend/api/[context]/latest/delegates/[delegateAddress].ts
+++ packages/backend/api/[context]/latest/delegates/[delegateAddress].ts
@@ -1,3 +1,46 @@
 // return the address's delegating to this delegateAddress, and their vote weight
 // from the snapshot table in the database (where the snapshot was created
 // for the given context and blocknumber is null)
+
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+import { db } from "../../../../lib/services/storage/db"
+import { BigNumber, ethers } from "ethers"
+
+export default async function getDelegations(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  const context = request.query.context as string
+  const delegateAddress = ethers.utils.getAddress(
+    request.query.delegateAddress as string,
+  )
+  console.log("context", context)
+  console.log("delegateAddress", delegateAddress)
+  const delegators = await db
+    .selectFrom("delegation_snapshot")
+    .where("context", "=", context)
+    .where("main_chain_block_number", "is", null)
+    .where("to_address", "=", delegateAddress)
+    .select(["from_address", "delegated_amount", "to_address_own_amount"])
+    .execute()
+
+  if (delegators.length === 0) {
+    console.log("No delegators found for delegateAddress", delegateAddress)
+  }
+
+  const voteWeightDelegated = delegators.reduce(
+    (acc, { delegated_amount }) => acc.add(BigNumber.from(delegated_amount)),
+    BigNumber.from(0),
+  )
+  const delegatesOwnVoteWeight = BigNumber.from(
+    delegators[0]?.to_address_own_amount ?? "0",
+  )
+
+  response.status(200).json({
+    delegators,
+    voteWeightDelegated: voteWeightDelegated.toString(),
+    numberOfDelegators: delegators.length,
+    delegatesOwnVoteWeight: delegatesOwnVoteWeight.toString(),
+    totalVoteWeight: voteWeightDelegated.add(delegatesOwnVoteWeight).toString(),
+  })
+}
--- packages/backend/api/[context]/latest/delegators/[delegatorAddress].ts
+++ packages/backend/api/[context]/latest/delegators/[delegatorAddress].ts
@@ -1,1 +1,10 @@
 // return a delegators delegation set from the graph
+
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+export default async function getDelegations(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  response.status(200).json({})
+}
