a6c83d7a7dae5e5cc8c29289ee0aa6d6292ce5c7
Asgeir <asgeir@cluda.com>
2023-06-08 12:35:31 +0200

    Update top delegates

--- packages/backend/api/[space]/latest/delegates/top.ts
+++ packages/backend/api/[space]/latest/delegates/top.ts
@@ -1,34 +1,49 @@
+// retun top delegates for a given snapshot space.
 import * as R from "ramda"
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 import { db, getDelegationSnapshot } from "../../../../lib/services/storage/db"
 
-// TODO: WIP
+const { count, sum } = db.fn
+
 export default async function getTopDelegates(
   request: VercelRequest,
   response: VercelResponse,
 ) {
   const space = request.query.space as string
+  const by: "count" | "weight" =
+    request.query.by === "weight" ? "weight" : "count"
 
-  const stats = await db
-    .selectFrom("delegation_snapshot")
-    .where("context", "=", space)
-    .where("main_chain_block_number", "is", null)
-    .select(["to_address"])
-    .execute()
+  let topDelegates
+  if (by === "count") {
+    topDelegates = await db
+      .selectFrom("delegation_snapshot")
+      .where("context", "=", space)
+      .where("main_chain_block_number", "is", null)
+      .groupBy("to_address")
+      .select(["to_address", count("to_address").as("number_of_delegations")])
+      .orderBy("number_of_delegations", "desc")
+      .execute()
+  } else if (by === "weight") {
+    topDelegates = await db
+      .selectFrom("delegation_snapshot")
+      .where("context", "=", space)
+      .where("main_chain_block_number", "is", null)
+      .groupBy("to_address")
+      .select(["to_address", sum("delegated_amount").as("delegated_amount")])
+      .orderBy("delegated_amount", "desc")
+      .execute()
+  } else {
+    throw new Error("Error: invalid 'by' parameter.")
+  }
 
-  if (stats.length === 0) {
+  if (topDelegates.length === 0) {
     console.log("No delegations found for space context", space)
   }
 
-  console.log(stats)
-  const finalArray = stats.map(function (obj) {
-    return obj.to_address
-  })
-  console.log(finalArray)
-  const test = R.countBy(R.toLower)(finalArray)
-  console.log(test)
+  console.log(topDelegates)
 
   response.status(200).json({
     success: "true",
+    topDelegates,
   })
 }
