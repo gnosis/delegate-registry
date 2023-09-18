465f14f51c606a25b20a7a4e09c73d6565a72f75
Asgeir <asgeir@cluda.com>
2023-09-18 10:22:05 +0200

    Add js version of resolvers (that are not working on Vercel, but works locally)

---
+++ packages/backend/lib/services/the-graph/resolvers.js
@@ -0,0 +1,8 @@
+export const resolvers = {
+  DelegationSet: {
+    chainName: (root, args, context, info) => context.chainName || "gorli", // The value we provide in the config
+  },
+  Context: {
+    chainName: (root, args, context, info) => context.chainName || "gorli", // The value we provide in the config
+  },
+}
