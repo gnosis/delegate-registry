cb252d6e00db9d00233ce2e68fd4b2b64f984c01
Asgeir <asgeir@cluda.com>
2023-11-06 08:44:53 +0100

    Improve test

--- packages/backend/lib/services/the-graph/index.test.ts
+++ packages/backend/lib/services/the-graph/index.test.ts
@@ -3,14 +3,13 @@ import { expect } from "chai"
 import { fetchDelegationSetsFromV1 } from "../../data"
 
 describe("fetchDelegationSetsFromV1", () => {
-  it("should do something that takes a long time", async function () {
+  it("should be possible to get V1 delegations", async function () {
     this.timeout(10000) // Set timeout to 5 seconds
     // Your test code here
     const chainId = 1
     const spaceName = "gnosis.eth"
     const result = await fetchDelegationSetsFromV1(spaceName, chainId)
 
-    console.log("result:", result)
     expect(result).to.be.an("array").that.is.not.empty
     expect(result[0]).to.have.property("denominator").equal("1")
     expect(result[0]).to.have.property("creationTimestamp").equal("0")
